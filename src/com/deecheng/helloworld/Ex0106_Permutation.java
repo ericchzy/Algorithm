0ba870a3334a259c46af30c321566bfce0bd5cfa
zyching <alpha.shining@gmail.com>
2018-07-02 11:03:37 +0800

    Implementation of String Permutation.

--- src/com/deecheng/helloworld/Ex0106_Permutation.java
+++ src/com/deecheng/helloworld/Ex0106_Permutation.java
@@ -9,10 +9,45 @@ package com.deecheng.helloworld;
 
 public class Ex0106_Permutation {
     public static void permute(char[] str, int low, int high) {
-
+        if (high <= 1) {
+            return;
+        }
+        if (low == high) {
+            System.out.println(str);
+        } else {
+            for (int i = low; i <= high; i++) {
+                swap(str, low, i);
+                permute(str, low + 1, high);
+                swap(str, i, low);
+            }
+        }
 
 
     }
 
-    public static void swap()
+    private static void swap(char[] str, int left, int right) {
+        char tmp = str[left];
+        str[left] = str[right];
+        str[right] = tmp;
+    }
 }
+
+class Ex0106_PermutationTestProgram {
+    public static void main(String[] args) {
+        char[] charArray = {'a', 'b', 'c'};
+        System.out.println(charArray.length);
+        Ex0106_Permutation.permute(charArray, 0 , charArray.length-1);
+    }
+}
+/* Output:
+3
+abc
+acb
+bac
+bca
+cba
+cab
+*///:~
+
+
+
