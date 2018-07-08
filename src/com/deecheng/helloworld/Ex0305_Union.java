33d5d66f7aef226293ae99fb870da45d3b1f5f39
cdee <zycheng.gm@gmail.com>
2018-07-08 17:21:03 +0800

    Add intersection and union implementation.

--- src/com/deecheng/helloworld/Ex0305_Union.java
+++ src/com/deecheng/helloworld/Ex0305_Union.java
@@ -1,13 +1,13 @@
 package com.deecheng.helloworld;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class Ex0304_Intersection {
+public class Ex0305_Union {
     public static <AnyType extends Comparable<? super AnyType>>
-    void intersection(List<AnyType> L1, List<AnyType> L2, List<AnyType> Intersect) {
+    void union(List<AnyType> L1, List<AnyType> L2, List<AnyType> unionResult) {
+
         ListIterator<AnyType> iterL1 = L1.listIterator();
         ListIterator<AnyType> iterL2 = L2.listIterator();
         AnyType itemL1=null, itemL2=null;
@@ -23,20 +23,29 @@ public class Ex0304_Intersection {
             int compareResult = itemL1.compareTo(itemL2);
             if ( compareResult == 0 )
             {
-                Intersect.add(itemL1);
+                unionResult.add(itemL1);
                 itemL1 = iterL1.hasNext()?iterL1.next():null;
                 itemL2 = iterL2.hasNext()?iterL2.next():null;
             }
-
-            //Notice that this two list is in order.
             else if ( compareResult < 0 )
             {
+                unionResult.add(itemL1);
                 itemL1 = iterL1.hasNext()?iterL1.next():null;
             }
             else
             {
+                unionResult.add(itemL2);
                 itemL2 = iterL2.hasNext()?iterL2.next():null;
             }
+        }
+
+        while (itemL1 != null) {
+            unionResult.add(itemL1);
+            itemL1 = iterL1.hasNext()?iterL1.next():null;
+        }
+        while (itemL2 != null) {
+            unionResult.add(itemL2);
+            itemL2 = iterL2.hasNext()?iterL2.next():null;
         }
     }
 
@@ -48,19 +57,18 @@ public class Ex0304_Intersection {
         for (int i = 0; i < 10; i++) {
             l1.add(i);
         }
-        for (int i = 0; i < 5; i++) {
+        for (int i = 3; i < 10; i++) {
             l2.add(i * 2);
         }
-        intersection(l1, l2, l3);
+        union(l1, l2, l3);
         System.out.println("l1: " + l1);
         System.out.println("l2: " + l2);
-        System.out.println("intersection: " + l3);
+        System.out.println("union set: " + l3);
     }
 }
 
 /* Output:
 l1: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-l2: [0, 2, 4, 6, 8]
-intersection: [0, 2, 4, 6, 8]
+l2: [6, 8, 10, 12, 14, 16, 18]
+union set: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18]
 *///:~
-
